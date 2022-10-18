e53c04092ac988e20919f9a76e02ecad7f451aca
Hoseung Lee <astrohsy@gmail.com>
2022-10-18 00:29:16 -0400

    Fix grouppage

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,25 @@
 <template>
   <q-page padding>
     <!-- content -->
-    This is login page!
+    <div class="row login-page">
+      <div class="col-3 div-3">1</div>
+      <div class="col-auto">1</div>
+      <!--
+     we have 3 children, so equivalent
+     to above would be to use `col-4`
+     on each of the children
+  -->
+    </div>
   </q-page>
 </template>
+<style>
+.login-page {
+  min-height: inherit
+}
+ .div-3 {
+    	background-color: rgb(255,0,0);
+    }
+</style>
 
 <script>
 export default {
