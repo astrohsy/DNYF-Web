7ae65d7e35e50175f4b66507f757cb7610ae6b2e
Hoseung Lee <astrohsy@gmail.com>
2022-12-13 12:07:16 -0500

    fix: creat user

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,27 +4,29 @@ import { api } from "src/boot/axios";
 export const useUserStore = defineStore("user", {
   state: () => ({
     user: {},
+    created: false,
   }),
   getters: {},
   actions: {
     async createUser(config, data) {
+      console.log(data);
       try {
-        const response = await api.post("/user", data, (config = config));
-        const groups = response.data.data;
-        console.log(`/groups: ${JSON.stringify(groups)}`);
+        const response = await api.post("/users", data, (config = config));
+        const users = response.data;
+        console.log(`/users: ${JSON.stringify(users)}`);
       } catch (e) {
         console.log(e);
       }
     },
     async fetchUser(config, id) {
       try {
-        id = "sample1";
         const response = await api.get(`/users/${id}`, (config = config));
         const user = response.data;
 
         console.log(`/users/{id}: ${JSON.stringify(user)}`);
         console.log(user);
         this.user = user;
+        this.created = true;
       } catch (e) {
         console.log(e);
       }
